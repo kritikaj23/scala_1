8c066b7ab3162e1bf0b538224f799e5a77835d6e
kritika <jkritika223@gmail.com>
2023-08-12 11:04:50 +0530

    Initial commit1111

---
+++ src/test/scala/first/first.scala
@@ -0,0 +1,14 @@
+package first
+
+class first {
+  // Scala program to print Hello World!
+  object Geeks {
+    // Main Method
+    def main(args: Array[String]) {
+      // prints Hello World
+      println("Hello World!")
+    }
+  }
+
+
+}
---
+++ src/test/scala/first/kritika.scala
@@ -0,0 +1,12 @@
+package first
+
+object kritika { // Scala program to print Hello World!
+
+    // Main Method
+    def main(args: Array[String]) {
+      // prints Hello World
+      println("Hello World!")
+    }
+  }
+
+
